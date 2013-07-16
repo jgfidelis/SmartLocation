4725ce226e23e29680286eb1dc6124e3fb21d0f4
Zhenlei Ji <zhenlei.ji@gmail.com>
2013-07-16 00:22:35 -0300

    Adding generate stub on settings. (not implemented yet)

--- src/com/zhji/location/app/views/MainActivity.java
+++ src/com/zhji/location/app/views/MainActivity.java
@@ -6,22 +6,16 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.Menu;
-import android.view.View;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import com.google.android.gms.location.Geofence;
 import com.zhji.location.app.R;
-import com.zhji.location.app.db.DatabaseManager;
-import com.zhji.location.app.db.model.LocationDatabase;
 import com.zhji.location.app.services.SmartLocationService;
 import com.zhji.location.app.services.SmartLocationService.LocalBinder;
-
-import java.util.List;
-import java.util.Random;
 
 public class MainActivity extends Activity implements ServiceConnection {
 
@@ -34,13 +28,6 @@ public class MainActivity extends Activity implements ServiceConnection {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "Starting Smart Location Service...");
         startService(new Intent(this, SmartLocationService.class));
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(final Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
     }
 
     @Override
@@ -59,33 +46,6 @@ public class MainActivity extends Activity implements ServiceConnection {
         super.onStop();
     }
 
-    public void doSomething(final View view) {
-        AsyncTask.execute(new Runnable() {
-
-            @Override
-            public void run() {
-                final LocationDatabase database = DatabaseManager.getInstance()
-                        .getLocationDatabase();
-                // final double l = 45;
-                // database.insert(LocationDatabase.toContentValues(l, l));
-
-                // database.insert(LocationDatabase.toContentValues(new
-                // Random().nextDouble() * 90,
-                // new Random().nextDouble() * 90));
-
-                for (int i = 0; i < 50; i++) {
-                    database.insert(LocationDatabase.toContentValues(
-                            new Random().nextDouble() * 90,
-                            new Random().nextDouble() * 90));
-                }
-
-                final List<Geofence> geofences = database.convertToGeofences();
-                Log.d(TAG, geofences != null ? geofences.toString() : "");
-            }
-        });
-        return;
-    }
-
     @Override
     public void onServiceConnected(final ComponentName name, final IBinder service) {
         final LocalBinder binder = (LocalBinder) service;
@@ -97,5 +57,65 @@ public class MainActivity extends Activity implements ServiceConnection {
     @Override
     public void onServiceDisconnected(final ComponentName name) {
         Log.d(TAG, "Smart Location Service disconnected...");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Toast.makeText(this, "Settings not implemented.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_generate_stub_location:
+                generateStubLocation();
+                break;
+            case R.id.action_generate_stub_geofence:
+                generateStubGeofence();
+                break;
+            case R.id.action_generate_stub_activity:
+                generateStubActivityRecognition();
+                break;
+            case R.id.action_generate_stub_all:
+                generateStubLocation();
+                generateStubGeofence();
+                generateStubActivityRecognition();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * This method populate the location database with stub data
+     */
+    private void generateStubLocation() {
+        // TODO Auto-generated method stub
+        Toast.makeText(this, "Generate stub location not implemented.", Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    /**
+     * This method populate the geofence database with stub data
+     */
+    private void generateStubGeofence() {
+        // TODO Auto-generated method stub
+        Toast.makeText(this, "Generate stub geofence not implemented.", Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    /**
+     * This method populate the activity recognition database with stub data
+     */
+    private void generateStubActivityRecognition() {
+        // TODO Auto-generated method stub
+        Toast.makeText(this, "Generate stub activity recognition not implemented.",
+                Toast.LENGTH_SHORT).show();
     }
 }
